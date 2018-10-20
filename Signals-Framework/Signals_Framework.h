f67d9ca98341525193cf692440c827e2306026a6
Bill Abt <babt@me.com>
2018-10-20 09:09:43 -0400

    Publish shared scheme for Carthage.

---
+++ Signals-Framework/Signals_Framework.h
@@ -0,0 +1,18 @@
+//
+//  Signals_Framework.h
+//  Signals-Framework
+//
+//  Created by Bill Abt on 10/19/18.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Signals_Framework.
+FOUNDATION_EXPORT double Signals_FrameworkVersionNumber;
+
+//! Project version string for Signals_Framework.
+FOUNDATION_EXPORT const unsigned char Signals_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Signals_Framework/PublicHeader.h>
+
+
