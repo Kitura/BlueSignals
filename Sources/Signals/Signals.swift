e1c8033ea0ac30ab423afe94cbf6c7380dd2b664
Guang1234567 <lihansey163@163.com>
2021-08-25 15:26:59 +0800

    [Android] support.

--- Sources/Signals/Signals.swift
+++ Sources/Signals/Signals.swift
@@ -19,9 +19,9 @@
 //
 
 #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-	import Darwin
-#elseif os(Linux)
-	import Glibc
+import Darwin
+#elseif os(Linux) || os(Android)
+import Glibc
 #endif
 
 import Foundation
@@ -29,172 +29,188 @@ import Foundation
 // MARK: Signals
 
 public class Signals {
-	
-	// MARK: Enums
-	
-	///
-	/// Common OS Signals
-	///
-	public enum Signal {
-		case hup
-		case int
-		case quit
-		case abrt
-		case kill
-		case alrm
-		case term
-		case pipe
-		case user(Int)
-		
-		///
-		/// Obtain the OS dependent value of a Signal
-		///
-		public var valueOf: Int32 {
-			
-			switch self {
-			case .hup:
-				return Int32(SIGHUP)
-			case .int:
-				return Int32(SIGINT)
-			case .quit:
-				return Int32(SIGQUIT)
-			case .abrt:
-				return Int32(SIGABRT)
-			case .kill:
-				return Int32(SIGKILL)
-			case .alrm:
-				return Int32(SIGALRM)
-			case .term:
-				return Int32(SIGTERM)
-			case .pipe:
-				return Int32(SIGPIPE)
-			case .user(let sig):
-				return Int32(sig)
-				
-			}
-		}
-	}
-	
 
-	// MARK: Typealiases
-	
-	///
-	/// Action handler signature.
-	///
-	public typealias SigActionHandler = @convention(c)(Int32) -> Void
+    // MARK: Enums
+
+    ///
+    /// Common OS Signals
+    ///
+    public enum Signal {
+        case hup
+        case int
+        case quit
+        case ill
+        case abrt
+        case kill
+        case pipe
+        case alrm
+        case term
+        case user(Int)
+
+        ///
+        /// Obtain the OS dependent value of a Signal
+        ///
+        public var valueOf: CInt {
+
+            switch self {
+            case .hup:
+                return CInt(SIGHUP)
+            case .int:
+                return CInt(SIGINT)
+            case .quit:
+                return CInt(SIGQUIT)
+            case .ill:
+                return CInt(SIGILL)
+            case .abrt:
+                return CInt(SIGABRT)
+            case .kill:
+                return CInt(SIGKILL)
+            case .pipe:
+                return CInt(SIGPIPE)
+            case .alrm:
+                return CInt(SIGALRM)
+            case .term:
+                return CInt(SIGTERM)
+            case .user(let sig):
+                return CInt(sig)
+
+            }
+        }
+    }
 
 
-	// MARK: Class Methods
-	
-	///
-	/// Trap an operating system signal.
-	///
-	/// - Parameters:
-	///		- signal:	The signal to catch.
-	///		- action:	The action handler.
-	///
-	public class func trap(signal: Signal, action: @escaping SigActionHandler) {
-	
-		#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+    // MARK: Typealiases
 
-			var signalAction = sigaction(__sigaction_u: unsafeBitCast(action, to: __sigaction_u.self), sa_mask: 0, sa_flags: 0)
-		
-			_ = withUnsafePointer(to: &signalAction) { actionPointer in
-				
-				sigaction(signal.valueOf, actionPointer, nil)
-			}
-		
-		#elseif os(Linux)
-	
-			var sigAction = sigaction()
-	
-			sigAction.__sigaction_handler = unsafeBitCast(action, to: sigaction.__Unnamed_union___sigaction_handler.self)
-	
-			_ = sigaction(signal.valueOf, &sigAction, nil)
-	
-		#endif
-	}
-	
-	///
-	/// Trap multiple signals to individual handlers
-	///
-	/// - Parameter signals:	An array of tuples each containing a signal and signal handler.
-	///
-	public class func trap(signals: [(signal: Signal, action: SigActionHandler)]) {
-	
-		for sighandler in signals {
-			
-			Signals.trap(signal: sighandler.signal, action: sighandler.action)
-		}
-	}
-	
-	///
-	/// Trap multiple signals to a single handler
-	///
-	/// - Parameters:
-	///		- signals:	An array of signals to catch.
-	///		- action:	The action handler that will handle these signals.
-	///
-	public class func trap(signals: [Signal], action: @escaping SigActionHandler) {
-		
-		for signal in signals {
-			
-			Signals.trap(signal: signal, action: action)
-		}
-	}
-	
-	///
-	/// Raise an operating system signal
-	///
-	/// - Parameter signal:	The signal to raise.
-	///
-	public class func raise(signal: Signal) {
-		
-		#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-		
-			_ = Darwin.raise(signal.valueOf)
-		
-		#elseif os(Linux)
-		
-			_ = Glibc.raise(signal.valueOf)
-		
-		#endif
-	}
-	
-	///
-	/// Ignore a signal
-	///
-	/// - Parameter signal:	The signal to ignore.
-	///
-	public class func ignore(signal: Signal) {
-		
-		#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-			
-			_ = Darwin.signal(signal.valueOf, SIG_IGN)
-			
-		#elseif os(Linux)
-			
-			_ = Glibc.signal(signal.valueOf, SIG_IGN)
-			
-		#endif
-	}
+    ///
+    /// Action handler signature.
+    ///
+    public typealias SigActionHandler = @convention(c) (CInt) -> Void
 
-	///
-	/// Restore default signal handling
-	///
-	/// - Parameter signal:	The signal to restore.
-	///
-	public class func restore(signal: Signal) {
-		
-		#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-			
-			_ = Darwin.signal(signal.valueOf, SIG_DFL)
-			
-		#elseif os(Linux)
-			
-			_ = Glibc.signal(signal.valueOf, SIG_DFL)
-			
-		#endif
-	}
-	
+
+    // MARK: Class Methods
+
+    ///
+    /// Trap an operating system signal.
+    ///
+    /// - Parameters:
+    ///		- signal:	The signal to catch.
+    ///		- action:	The action handler.
+    ///
+    public class func trap(signal: Signal, mask: sigset_t = 0, flags: CInt = 0, action: @escaping SigActionHandler) {
+
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+
+        var signalAction = sigaction(
+                __sigaction_u: __sigaction_u(__sa_handler: action),
+                sa_mask: mask,
+                sa_flags: flags
+        )
+
+        #elseif os(Linux)
+
+        var sigAction = sigaction(
+                __sigaction_handler: unsafeBitCast(action, to: sigaction.__Unnamed_union___sigaction_handler.self),
+                sa_flags: flags,
+                sa_mask: mask,
+                sa_restorer: nil
+        )
+
+        #elseif os(Android)
+
+        var sa = sigaction(
+                .init(sa_handler: handler),
+                sa_mask: mask,
+                sa_flags: flags,
+                sa_restorer: nil)
+
+        #endif
+
+        withUnsafePointer(to: &signalAction) { actionPointer in
+
+            sigaction(signal.valueOf, actionPointer, nil)
+        }
+    }
+
+    ///
+    /// Trap multiple signals to individual handlers
+    ///
+    /// - Parameter signals:	An array of tuples each containing a signal and signal handler.
+    ///
+    public class func trap(signals: [(signal: Signal, action: SigActionHandler, mask: sigset_t, flags: CInt)]) {
+
+        for item in signals {
+
+            Signals.trap(signal: item.signal, mask: item.mask, flags: item.flags, action: item.action)
+        }
+    }
+
+    ///
+    /// Trap multiple signals to a single handler
+    ///
+    /// - Parameters:
+    ///		- signals:	An array of signals to catch.
+    ///		- action:	The action handler that will handle these signals.
+    ///
+    public class func trap(signals: [Signal], mask: sigset_t = 0, flags: CInt = 0, action: @escaping SigActionHandler) {
+
+        for signal in signals {
+
+            Signals.trap(signal: signal, mask: mask, flags: flags, action: action)
+        }
+    }
+
+    ///
+    /// Raise an operating system signal
+    ///
+    /// - Parameter signal:	The signal to raise.
+    ///
+    public class func raise(signal: Signal) {
+
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+
+        _ = Darwin.raise(signal.valueOf)
+
+        #elseif os(Linux) || os(Android)
+
+        _ = Glibc.raise(signal.valueOf)
+
+        #endif
+    }
+
+    ///
+    /// Ignore a signal
+    ///
+    /// - Parameter signal:	The signal to ignore.
+    ///
+    public class func ignore(signal: Signal) {
+
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+
+        _ = Darwin.signal(signal.valueOf, SIG_IGN)
+
+        #elseif os(Linux) || os(Android)
+
+        _ = Glibc.signal(signal.valueOf, SIG_IGN)
+
+        #endif
+    }
+
+    ///
+    /// Restore default signal handling
+    ///
+    /// - Parameter signal:	The signal to restore.
+    ///
+    public class func restore(signal: Signal) {
+
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+
+        _ = Darwin.signal(signal.valueOf, SIG_DFL)
+
+        #elseif os(Linux) || os(Android)
+
+        _ = Glibc.signal(signal.valueOf, SIG_DFL)
+
+        #endif
+    }
+
 }
